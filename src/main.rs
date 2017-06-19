3528cde17e4f7cb5acc71a5be9cf43d0955af700
Donovan Schönknecht <don@tpyo.net>
2017-06-19 05:41:51 +0200

    Source cleanup

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,20 @@ use maxminddb::geoip2::model::{City, Country, Subdivision, Location};
 
 #[derive(Serialize)]
 struct Result {
-    ip: String,
-    latitude: f64,
-    longitude: f64,
-    time_zone: String,
-    iso_code: String,
-    city: BTreeMap<String, String>,
-    subdivisions: Vec<BTreeMap<String, String>>, 
-    country: BTreeMap<String, String>,
-    registered_country: BTreeMap<String, String>
+	ip: String,
+	latitude: f64,
+	longitude: f64,
+	time_zone: String,
+	iso_code: String,
+	city: BTreeMap<String, String>,
+	subdivisions: Vec<BTreeMap<String, String>>,
+	country: BTreeMap<String, String>,
+	registered_country: BTreeMap<String, String>
 }
 
 struct Server {
-    thread_pool: CpuPool,
-    reader: Arc<maxminddb::Reader>
+	thread_pool: CpuPool,
+	reader: Arc<maxminddb::Reader>
 }
 
 fn get_city_names(city: &Option<City>) -> BTreeMap<String, String> {
@@ -98,84 +98,82 @@ fn get_subdivision_names(subdivisions: &Option<Vec<Subdivision>>) -> Vec<BTreeMa
 }
 
 impl Service for Server {
-    type Request = Request;
-    type Response = Response;
-    type Error = io::Error;
-    type Future = BoxFuture<Response, io::Error>;
+	type Request = Request;
+	type Response = Response;
+	type Error = io::Error;
+	type Future = BoxFuture<Response, io::Error>;
 
-    fn call(&self, req: Request) -> Self::Future {
-       
-        let db = self.reader.clone();
-        let msg = self.thread_pool.spawn_fn(move || {
+	fn call(&self, req: Request) -> Self::Future {
+		let db = self.reader.clone();
+		let msg = self.thread_pool.spawn_fn(move || {
 			let path = req.path();
-        	let foo = &path[1..];
-        	//println!("Looking up {}", foo);
+			let foo = &path[1..];
+			//println!("Looking up {}", foo);
 
 			match IpAddr::from_str(foo) {
-			    Ok(ip) => {
-
-			    	match db.lookup(ip) {
-			    		Ok(xlookup) => {
-			    			let lookup: geoip2::City = xlookup;
-			    			let result = Result {
-				                ip: String::from_str(foo).unwrap(),
-				                longitude: get_location_longitude(&lookup.location),
-				                latitude: get_location_latitude(&lookup.location),
-				                time_zone: get_location_time_zone(&lookup.location),
-				                iso_code: get_country_iso_code(&lookup.country),
-				                city: get_city_names(&lookup.city),
-				                subdivisions: get_subdivision_names(&lookup.subdivisions),
-				                country: get_country_names(&lookup.country),
-				                registered_country: get_country_names(&lookup.registered_country),
-				            };
-				            Ok(serde_json::to_string(&result).unwrap())
-			    		},
-			    		Err(_) => Ok(String::from("{\"error\": \"not_found\"}"))
-			    	}
-			    },
-			    Err(AddrParseError(_)) => Ok(String::from("{\"error\": \"invalid_address\"}")),
+				Ok(ip) => {
+					match db.lookup(ip) {
+						Ok(xlookup) => {
+							let lookup: geoip2::City = xlookup;
+							let result = Result {
+								ip: String::from_str(foo).unwrap(),
+								longitude: get_location_longitude(&lookup.location),
+								latitude: get_location_latitude(&lookup.location),
+								time_zone: get_location_time_zone(&lookup.location),
+								iso_code: get_country_iso_code(&lookup.country),
+								city: get_city_names(&lookup.city),
+								subdivisions: get_subdivision_names(&lookup.subdivisions),
+								country: get_country_names(&lookup.country),
+								registered_country: get_country_names(&lookup.registered_country),
+							};
+							Ok(serde_json::to_string(&result).unwrap())
+						},
+						Err(_) => Ok(String::from("{\"error\": \"not_found\"}"))
+					}
+				},
+				Err(AddrParseError(_)) => Ok(String::from("{\"error\": \"invalid_address\"}")),
 			}
-        });
+		});
 
-        msg.map(|body| {
-            let mut response = Response::new();
-            response.header("Content-Type", "application/json; charset=UTF-8");
-            response.body(&body);
-            response
-        }).boxed()
-    }
+		msg.map(|body| {
+			let mut response = Response::new();
+			response.header("Content-Type", "application/json; charset=UTF-8");
+			response.body(&body);
+			response
+		}).boxed()
+	}
 }
 
 fn main() {
-    let args = args().collect::<Vec<_>>();
+	let args = args().collect::<Vec<_>>();
 
-    if args.len() < 3 {
-        println!("Usage: {} <bind ip:port> <mmdb file>", args[0]);
-        return;
-    }
+	if args.len() < 3 {
+		println!("Usage: {} <bind ip:port> <mmdb file>", args[0]);
+		return;
+	}
 
 	let addr = match SocketAddr::from_str(&args[1]) {
-	    Ok(addr) => addr,
-	    Err(AddrParseError(_)) => {
-	    	println!("Error: Invalid bind address: \"{}\".", &args[1]);
-	    	return
-	    }
+		Ok(addr) => addr,
+		Err(AddrParseError(_)) => {
+			println!("Error: Invalid bind address: \"{}\".", &args[1]);
+			return
+		}
 	};
 
-    let reader = match maxminddb::Reader::open(&args[2]) {
-    	Ok(reader) => reader,
-        Err(_) => {
-        	println!("Error: Unable to open database file: \"{}\"", &args[2]);
-        	return
-        }
-    };
+	let reader = match maxminddb::Reader::open(&args[2]) {
+		Ok(reader) => reader,
+		Err(_) => {
+			println!("Error: Unable to open database file: \"{}\"", &args[2]);
+			return
+		}
+	};
 
-    let db = Arc::new(reader);
-    let thread_pool = CpuPool::new(num_cpus::get());
-    TcpServer::new(tokio_minihttp::Http, addr).serve(move || {
-        Ok(Server {
-            thread_pool: thread_pool.clone(),
-            reader: db.clone(),
-        })
-    });
+	let db = Arc::new(reader);
+	let thread_pool = CpuPool::new(num_cpus::get());
+	TcpServer::new(tokio_minihttp::Http, addr).serve(move || {
+		Ok(Server {
+			thread_pool: thread_pool.clone(),
+			reader: db.clone(),
+		})
+	});
 }
